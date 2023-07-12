651cfc9b7cc1768f67499b1cbf6f35a2c08fb5c2
godot63 <lcalmbach@gmail.com>
2023-07-12 07:01:22 +0200

    enhance example text

--- const.py
+++ const.py
@@ -1,14 +1,12 @@
 LANG_FILE = "./lang/app.json"
 
-example_input = {"en": {"welcome": "Welcome", "menu": "Menu"}, "de": {}, "fr": {}}
+example_input = {"source": {"welcome": "Welcome", "menu": "Menu"}, "en": {}, "de": {}, "fr": {}}
 example_output = {
+    "source": {"welcome": "Welcome", "selection": "Selection"},
     "en": {"welcome": "Welcome", "selection": "Selection"},
     "de": {"welcome": "Willkommen", "selection": "Auswahl"},
     "fr": {"welcome": "Bienvue", "selection": "Sélection"},
-    "it": {"welcome": "Benvenuto"},
-    "zh": {"welcome": "歡迎"},
 }
-
 
 system_message = """You will translate strings kept in a json formatted input delimited by #### and generate a json formatted output.
 The json input file is formatted as in this example:
